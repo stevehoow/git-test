3de4ec7f8fbf146e7dc1eb40529c055906ba122a
stevehoow <35597444+stevehoow@users.noreply.github.com>
2022-10-01 15:46:45 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("master test!!!");
         System.out.println("hotfix test!!!");
         System.out.println("push test!!!");
+        System.out.println("pull test!!!");
+
     }
 }
