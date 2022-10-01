58db367835c4823b5cc7c9160b1f6229d135f572
Layne <Layne@gmail.com>
2022-10-01 18:24:53 +0800

    push3 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("pull test!!!");
         System.out.println("push2 test!!!");
         System.out.println("pull test2!!!");
+        System.out.println("push3 test!!!");
 
     }
 }
